7e77241dedc67c81525b0470b9a0d05b6ee93926
Thiago Dias <133836154+thiagofddias@users.noreply.github.com>
2024-08-19 10:58:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ console.log(reverseStringInPairs("abcd"));
 console.log(reverseStringInPairs("abcdef"));
 console.log(reverseStringInPairs("ab"));
 console.log(reverseStringInPairs(""));
+
+// Nesse código, a função define a variavel result como null, então cria uma estrutura de repetição, definindo a variavel "s", que representa a string 
+// como 0, e vai criando mais 2 parâmetros, sendo eles: continue se o valor de "s" for menor que o lenght da input que será retornada e faça a verificação 
+// de "s" a cada 2 caracteres, fazendo que o código leia os pares de caracteres, depois disso ele vai concatenando os valores, por exemplo: "a" + "b" = "ab"
+// "c" + "d" = "cd", então ele concatena o ultimo valor gerado com os valores gerados anteriormente, portanto caso o valor de input seja "abcd", ele vai retornar
+// "cdab", caso seja "abcdef", ele vai retornar "efcdab"
